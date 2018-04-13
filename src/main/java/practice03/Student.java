1cd30d27f8dfe4349f4d3f9e30bda05c75beaea3
wan_wansi <576529493@qq.com>
2018-04-13 22:13:41 +0800

    add practice03

---
+++ src/main/java/practice03/Student.java
@@ -0,0 +1,17 @@
+package practice03;
+
+public class Student extends Person {
+    private int klass;
+
+    public Student(String name, int age,int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public int getKlass() {
+        return klass;
+    }
+    public String introduce() {
+        return ("I am a Student. I am at Class "+klass+".");
+    }
+}
