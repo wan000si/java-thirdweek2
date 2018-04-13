b1febae48e32ac5f615981d148bb41f620a0d501
wan_wansi <576529493@qq.com>
2018-04-13 21:43:18 +0800

    add Person.java

---
+++ src/main/java/practice01/Person.java
@@ -0,0 +1,21 @@
+package practice01;
+
+public class Person {
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+    public String getName() {
+        return name;
+    }
+    public int getAge() {
+        return age;
+    }
+
+    public String introduce(String name, int age) {
+       return ("My name is "+name+". I am "+age+" years old.");
+    }
+}
