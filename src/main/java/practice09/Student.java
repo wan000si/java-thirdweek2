f9f182a3a92b0b5300806494131a5c722697c0f1
wan_wansi <576529493@qq.com>
2018-04-14 17:19:37 +0800

    add practice09

---
+++ src/main/java/practice09/Student.java
@@ -0,0 +1,30 @@
+package practice09;
+
+public class Student extends Person{
+    private Klass klass;
+
+    public Student(int id, String name, int age) {
+        super(id, name, age);
+    }
+
+    public Student(int id, String name, int age,Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+
+    public Klass setKlass(Klass klass) {
+        return klass;
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public String introduce() {
+        if (klass.getLeader() == this) {
+            return (super.introduce() + " I am a Student. I am Leader of Class " + getKlass().getNumber() + ".");
+        } else {
+            return (super.introduce() + " I am a Student. I am at Class " + getKlass().getNumber() + ".");
+        }
+    }
+}
