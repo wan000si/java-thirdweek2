f2c2f89c2795d7348d1255dc8ad0992462864231
wan_wansi <576529493@qq.com>
2018-04-14 10:58:55 +0800

    add practice07

---
+++ src/main/java/practice07/Student.java
@@ -0,0 +1,16 @@
+package practice07;
+
+public class Student extends Person {
+
+    private Klass klass;
+    public Student(String name, int age,Klass klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+    public Klass getKlass() {
+        return klass;
+    }
+    public String introduce() {
+        return (super.introduce()+" I am a Student. I am at Class "+getKlass().getNumber()+".");
+    }
+}
---
+++ src/main/java/practice07/Teacher.java
@@ -0,0 +1,32 @@
+package practice07;
+
+public class Teacher extends Person {
+    private Klass klass;
+
+    public Teacher(String name, int age,Klass klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+    public String introduce() {
+        if (klass!= null ) {
+            return (super.introduce() + " I am a Teacher. I teach Class " + getKlass().getNumber() + ".");
+        } else {
+            return (super.introduce() + " I am a Teacher. I teach No Class.");
+        }
+    }
+    public String introduceWith(Student student ) {
+        if (this.klass.equals(student.getKlass())) {
+            return (super.introduce() + " I am a Teacher. I teach " + student.getName() + ".");
+        } else {
+            return (super.introduce()+" I am a Teacher. I don't teach "+student.getName()+".");
+        }
+    }
+}
