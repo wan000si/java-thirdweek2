12977791848df35393ba797614e28d470b604f45
wan_wansi <576529493@qq.com>
2018-04-14 10:44:36 +0800

    add practice06

---
+++ src/main/java/practice06/Person.java
@@ -0,0 +1,20 @@
+package practice06;
+
+public class Person {
+    private String name;
+    private int age;
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+    public String getName() {
+        return name;
+    }
+    public int getAge() {
+        return age;
+    }
+
+    public String introduce() {
+        return ("My name is "+name+". I am "+age+" years old.");
+    }
+}
---
+++ src/main/java/practice06/Teacher.java
@@ -0,0 +1,25 @@
+package practice06;
+
+public class Teacher extends Person {
+    private int klass;
+
+    public Teacher(String name, int age,int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public int getKlass() {
+        return klass;
+    }
+    public String introduce() {
+        if (klass == 0) {
+            return (super.introduce()+" I am a Teacher. I teach No Class.");
+        }
+        return (super.introduce() + " I am a Teacher. I teach Class " + getKlass() + ".");
+
+    }
+}
